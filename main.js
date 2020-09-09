5818dd49b646559da385363a1147c9601f49650c
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-09 18:44:51 +1000

    solution for animate problem

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 let ele = document.querySelector('#el2');
-let b = document.querySelector('body');
 
 console.log(ele);
 
 function animate(el, millisec, distance) {
-  let style = document.createElement('style');
-  style.type = 'text/css';
-  let keyframe = `@keyframes move {
-        from {top: 0px;}
-        to {top: 200px;}
-    }`;
-  style.innerHTML = keyframe;
-  document.head.appendChild(style);
+  // Edited CSS keyframe solution
+  //   let style = document.createElement('style');
+  //   style.type = 'text/css';
+  //   let keyframe = `@keyframes move {
+  //         from {top: 0px;}
+  //         to {top: ${distance}px;}
+  //     }`;
+  //   style.innerHTML = keyframe;
+  //   document.head.appendChild(style);
 
-  el.setAttribute('style', `position: relative; animation: move 3s ease`);
+  //   el.setAttribute(
+  //     'style',
+  //     `position: relative; animation: move ${millisec / 1000}s ease`
+  //   );
+
+  //   Interval solution
+  el.style.position = 'relative';
+  el.style.top = 0;
+  let times = millisec / 10;
+  let portionDis = distance / times;
+  let currentPos = 0;
+
+  console.log(times, portionDis, el.style.top);
+
+  const moving = setInterval(() => {
+    if (currentPos >= distance) {
+      clearInterval(moving);
+    }
+    currentPos += portionDis;
+    el.style.top = `${currentPos}px`;
+  }, millisec / times);
 }
 
-animate(ele, 2000, 400);
+animate(ele, 2000, 800);
