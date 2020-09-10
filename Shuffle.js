2e1f744ebf7ad68e51f64f42baa1545fc91a0718
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-10 17:35:13 +1000

    added more problems

--- Shuffle.js
+++ Shuffle.js
@@ -35,3 +35,11 @@ function shuffle(array) {
     }
   }
 }
+
+// Array shuffle
+for (let i = array.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * i);
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
